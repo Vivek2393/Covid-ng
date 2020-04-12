0b6ef3d1be5d04b333b33fe8a2e5539b66e3bcff
Vivek <blougupvivek@gmail.com>
2020-04-13 00:54:07 +0530

    Address Card Component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { AddressCardComponent } from './address-card/address-card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    AddressCardComponent
   ],
   imports: [
     BrowserModule,
